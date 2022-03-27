9ab3ad15b13cea5fb0b8f6cc8bac224fa8cc0287
남지원 <51081373+Bantree96@users.noreply.github.com>
2022-03-27 21:08:13 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BmpToJpg.ViewModels;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace BmpToJpg
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel _mainViewModel;
+        public MainWindow(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+            InitializeComponent();
+
+            this.DataContext = _mainViewModel;
+        }
+
+
+        private void Btn_Select_Path(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Btn_SelectFolder();
+        }
+
+        private void Btn_Save_Path(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Btn_SaveFolder();
+        }
+
+        private void Btn_Convert(object sender, RoutedEventArgs e)
+        {
+            _mainViewModel.Bmp2Jpg();
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using PropertyChanged;
+
+namespace BmpToJpg.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainViewModel
+    {
+        #region Field
+        private FolderBrowserDialog FBD;
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+
+        }
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// 선택한 폴더 경로
+        /// </summary>
+        public string SelectedPath { get; set; } = null;
+        public string SavePath { get; set; } = null;
+        /// <summary>
+        /// 압축률 
+        /// </summary>
+        public long Quality { get; set; } = 0;
+        #endregion
+
+        #region Button_Method
+        public void Btn_SelectFolder()
+        {
+            FBD = new FolderBrowserDialog();
+            FBD.ShowDialog();
+
+            SelectedPath = FBD.SelectedPath;
+
+            // 초기경로 DeskTop
+            /*
+            if (SelectedPath == "")
+            {
+                SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+
+            // 기본 경로를 txt로 저장
+            
+            using (StreamWriter sw = File.CreateText(AppDomain.CurrentDomain.BaseDirectory + @"/DefaultPath.txt"))
+            {
+                sw.WriteLine(selectedPath);
+            }
+            */
+        }
+
+        public void Btn_SaveFolder()
+        {
+            FBD = new FolderBrowserDialog();
+            FBD.ShowDialog();
+
+            SavePath = FBD.SelectedPath;
+        }
+        #endregion
+
+        #region Method
+        public void Bmp2Jpg()
+        {
+            DirectoryInfo di = new DirectoryInfo(SelectedPath);
+            // TODO : 2. 이미지 경로에 있는 이미지 파일을 가져오기
+            try
+            {
+                foreach (var item in di.GetFiles())
+                {
+                    // 파일이 없을 때
+                    if (item == null)
+                    {
+                        // 파일이 없습니다.
+                    }
+
+                    // 파일이 있을 때
+
+
+                    using (Bitmap inputImage = new Bitmap($@"{SelectedPath}\{item}"))
+                    {
+                        ImageCodecInfo info = ImageCodecInfo.GetImageEncoders().Where(a => a.MimeType.Contains("jpeg")).First();
+                        EncoderParameters eParams = new EncoderParameters(1);
+                        eParams.Param[0] = new EncoderParameter(Encoder.Quality, Quality);
+                        inputImage.Save($@"{SavePath}\{item}", info, eParams);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+      
+
+            // TODO : 3. JPG이미지로 변환하기
+
+           
+                
+          
+        }
+        #endregion
+    }
+
+
+}
